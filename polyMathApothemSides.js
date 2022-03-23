73a990f202bb0438ec2f855fc260c584b0ad06be
David Perez <javadavidperez@gmail.com>
2022-03-22 19:27:08 -0500

    some mods to include object functionality

--- polyMathApothemSides.js
+++ polyMathApothemSides.js
@@ -1,25 +1,43 @@
+//please notice: the order of the parameters provided to the function matters to match the correct propiety
+// if there is an unknown propiety of the polygon enter it as 1 and then run the method to find it-
+// each function will replace the missing value entered as 1 for the correct one
+class rPolygon {
+    constructor (pSides, apothem, sideLenght){
+        this.pSides = Number(pSides);
+        this.apothem = Number(apothem);
+        this.sideLenght = Number(sideLenght);
+    }
     //for regular polygons
+    //pSides = number of sides of the polygon
     //to find apothem we require side lenght and the total # of sides of the polygon
-    function polyApo (sideLenght, polySides){
-        let thetaAngle = 360/(2*polySides);
-        apothem = sideLenght/(2 *(Math.tan(thetaAngle*Math.PI/180)));
-        return apothem;
+    polyFindApothem(){
+        var pSides = this.pSides;
+        var sideLenght = this.sideLenght;
+        let thetaAngle = 360/(2*pSides);
+        let foundApothem = sideLenght/(2 *(Math.tan(thetaAngle*Math.PI/180)));
+        return (this.apothem = Math.round(foundApothem));
     }
     //Now only having the apothem and the #of sides of the polygon
-    function polygonExternalSideLenght(apothem, pSides){
+    polyFindSideLenght(){
+        var pSides = this.pSides;
+        var apothem = this.apothem;
         let thetaAngle = 360/(2*pSides); //finds the inner angle of the triangle to calculate the external side;
         let tanofAngle = Math.tan(thetaAngle*Math.PI/180);
         let halfsideLenght = tanofAngle * apothem;
-        return (halfsideLenght*2);
+        return (this.sideLenght = Math.round((halfsideLenght *2))); //this mettod will add the sideLenght to the object propieties.
     }
-    function polygonPerimeter(apothem, pSides){
+    polyPerimeter(){
+        var pSides = this.pSides;
+        var apothem = this.apothem;
         let thetaAngle = 360/(2*pSides); //finds the inner angle of the triangle to calculate the side
         let tanofAngle = Math.tan(thetaAngle*Math.PI/180);
         //Math.tan uses radians so above we convert the degrees to radians and we return the tan of the angle
         let halfsideLenght = tanofAngle * apothem;
         return (halfsideLenght*2) * pSides;
     }
-    function polygonArea(apothem, pSides){
+    polyArea(){
+        var pSides = this.pSides;
+        var apothem = this.apothem;
         let thetaAngle = 360/(2*pSides); 
         let tanofAngle = Math.tan(thetaAngle*Math.PI/180);
         let halfsideLenght = tanofAngle * apothem;
@@ -27,5 +45,12 @@
         let polyArea = (polyPerimeter * apothem/2);
         return polyArea;
     }
+}
 
+// if this hexagon have 6 sides will have an apo of 13 and a side lenght of 15
+hexagon = new rPolygon (6, 1, 15);
+hexagon.polyFindApothem(); //this will change the 1 for 13
+console.log(hexagon.apothem);
+console.log(hexagon.polyArea());
+console.log(hexagon.polyPerimeter());
 
